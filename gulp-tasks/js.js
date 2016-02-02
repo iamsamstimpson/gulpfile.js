c92c90cb6259d8d6f3572d13129445d5cb409f18
Jake Cobley <jake@cobe.ly>
2016-02-02 23:04:00 +0000

    removed sourcemaps from js since there's no functionailty to combine js files

--- gulp-tasks/js.js
+++ gulp-tasks/js.js
@@ -8,8 +8,6 @@ module.exports = function (project, gulp, nodeModule, environment) {
         return gulp.src(project.sourceDirectory + '/' + project.scriptsDirectory + '/**/*.js')
             .pipe(nodeModule.jsHint())
             .pipe(nodeModule.jsHint.reporter('default'))
-            .pipe(environment.development ? nodeModule.sourcemaps.init() : nodeModule.util.noop())
-            .pipe(environment.development ? nodeModule.sourcemaps.write() : nodeModule.util.noop())
             .pipe(environment.production ? nodeModule.uglify() : nodeModule.util.noop())
             .pipe(gulp.dest(project.distDirectory + '/' + project.scriptsDirectory))
             .pipe(nodeModule.browserSync.reload({stream: true}));
